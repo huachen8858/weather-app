ee08a5411493493fbbcb1d872a4320ffcd9af246
huachen8858 <huachen8858@gmail.com>
2024-02-18 16:22:07 +0800

    weather APP search city

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,70 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
+const api = {
+  key: process.env.NEXT_PUBLIC_OPENWEATHER_KEY,
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 export default function Home() {
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(
+        `${api.base}weather?q=${search}&units=metric&APPID=${api.key}`
+      );
+      const data = await response.json();
+      setSearch("")
+
+      if (data) {
+        // console.log(data);
+        setWeather(data);
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+
+  // useEffect(() => {
+  //   handleSearch();
+  // });
+
   return (
     <>
-      <h1>weather APP</h1>
+      <header>
+        <h1>weather APP</h1>
+        {/* Search Box */}
+        <div>
+          <input
+            type="text"
+            placeholder="Search city..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button onClick={handleSearch}>Search</button>
+        </div>
+        {typeof weather.main !== "undefined" ? (
+          <>
+            {/* Location */}
+            <p>{weather.name ? weather.name : ""}</p>
+            {/* Temperature */}
+            <p>{weather.main.temp ? weather.main.temp : 0}°C</p>
+            {/* Condition */}
+            <p>{weather.weather[0].main ? weather.weather[0].main : ""}</p>
+            <p>
+              (
+              {weather.weather[0].description
+                ? weather.weather[0].description
+                : ""}
+              )
+            </p>
+          </>
+        ) : (
+          ""
+        )}
+      </header>
     </>
   );
 }
